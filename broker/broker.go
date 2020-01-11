da50cfa1042603af33554a51723bcc9ef8b71fd2
ezio <iixelite@gmail.com>
2020-01-11 10:31:57 +0100

    Initial commit

---
+++ broker/broker.go
@@ -0,0 +1,293 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"net/rpc"
+	"time"
+)
+
+type QueueManager int
+
+const (
+	QUEUE_LEN = 100
+	CONSUMER_SERVICE_METHOD = "Consumer.Consume"
+
+	INVISIBLE = 0
+	VISIBLE = 1
+	SENT = 2
+)
+
+type QueueElement struct {
+	message string
+	visible map[int]int				//a map of consumer id to its value of visibility
+									//fot that message
+	lastTimeVisible int
+}
+
+func (q *QueueManager) Subscribe(ip string, port *string) error {
+
+	consumerID := consumerNum
+	consumerNum++
+	consumerList = append(consumerList, ConsumerInfo{ip, *port, consumerID})
+
+	*port = "Accepted"
+	//TODO check if the message is received
+
+	//if the strategy of keeping messages is chosen then
+	//when a new consume arrives, i have to check if there are
+	//pending messages
+
+	return nil
+}
+
+
+func (q *QueueManager) Unsubscribe(ip string, port *string) error {
+
+	consumerNum--
+
+	for i, consumer := range consumerList {
+		if consumer.port == *port && consumer.ip == ip {
+			*port = "Removed"
+			//this is not a critical section as long as
+			//the RPC server is sequential. It could happen that
+			//a consumer Unsubscribe but its unsubscribe request
+			//comes after a publish request of a producer.
+			consumerList[i] = consumerList[len(consumerList)-1]
+			consumerList = consumerList[:len(consumerList)-1]
+		}
+	}
+
+	return nil
+}
+
+
+func (q *QueueManager) Publish(message string, reply *string) error {
+
+	//actual strategy: if there are no consumer, reject the message
+	if consumerNum == 0 {
+		return nil
+	}
+
+	indx := findPosInQueue()
+
+	//if queue is full
+	if indx == -1 {
+		fmt.Println("\tWARNING: QUEUE IS FULL !")
+		*reply = "Queue is full !"
+		return nil
+	}
+
+	Queue[indx].lastTimeVisible = time.Now().Second()
+	Queue[indx].message = message
+	Queue[indx].visible = make(map[int]int)
+
+	//in this way each message will be sent to the consumer that
+	//are connected 'in this moment' to the broker
+	for _, c := range consumerList {
+		Queue[indx].visible[c.ID] = VISIBLE
+	}
+
+	for i := 0; i < consumerNum; i++ {
+		go sendToConsumer(indx, consumerList[i])
+	}
+
+	fmt.Println("Message received: "+message)
+	return nil
+}
+
+var TIME_OUT int
+var port string
+var Queue [QUEUE_LEN]QueueElement
+
+//consumerList is not a critical section as long as
+//the RPC server is sequential
+var consumerList []ConsumerInfo
+var consumerNum int
+
+type ConsumerInfo struct {
+	ip string
+	port string
+	ID int
+}
+
+
+func findPosInQueue() int{
+
+	for i, qe := range Queue {
+		if qe.lastTimeVisible == -1 {
+			return i
+		}
+	}
+
+	//if i'm here the queue is full
+	return -1
+}
+
+//a go routine that for each message to each consumer
+//reuse producer code... 3
+func sendToConsumer(queueElementIndex int, consumer ConsumerInfo) {
+
+	//changing visibility of the message for the current consumer to invisible
+	Queue[queueElementIndex].visible[consumer.ID] = INVISIBLE
+
+	client, err := rpc.Dial("tcp", consumer.ip+":"+consumer.port)
+
+	var reply string
+
+	//TODO the TIME OUT VALUE has to be chosen in order to avoid
+	//situations where i could send a message more than once.
+	//for simplicity, i am not handling this case.
+
+	//this case could be handled by the consumer
+
+	err = client.Call(CONSUMER_SERVICE_METHOD, Queue[queueElementIndex].message, &reply)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	//if i am not here the message has not arrived to the consumer
+	//and its value for its consumer remains 'invisible'
+
+	if reply == "ACK" {
+		//if i'm here the massage has been correctly delivered
+		Queue[queueElementIndex].visible[consumer.ID] = SENT
+
+		//now each consumer will check if the message could be eliminated
+		//by checking that all the values on the 'visible' map are 'sent'
+
+		del := true
+		for _, v := range Queue[queueElementIndex].visible {
+			//v is the value (invisible, sent, visible) of the message, for each consumer
+
+			if v != SENT {
+				del = false
+				break
+			}
+		}
+
+		if del {
+			//i am not using the synchronization, this means
+			//that when scanning, more than one go routine can
+			//realize that all the others has sent the message
+			//This means that more than one routine could set
+			//the lastTimeVisible = -1.
+			//It would have been a problem if each of
+			//them wanted to set a different value
+			Queue[queueElementIndex].lastTimeVisible = -1
+		}
+	}
+}
+
+func getConsumer(ID int) ConsumerInfo {
+
+	var c_nil ConsumerInfo
+	//the consumer could have been unsubscribed
+	//and could not be present anymore
+	c_nil.ID = -1
+
+	for _, c := range consumerList {
+		if c.ID == ID {
+			return c
+		}
+	}
+
+	//if am here i have to notify the caller that the consumer
+	//han not been found
+	return c_nil
+}
+
+//go routine that will implement the 'timed out based' delivery semantic
+func TimeOutChecker() {
+
+	var now int
+
+	for	{
+		time.Sleep(time.Second*time.Duration(TIME_OUT))
+
+		now = time.Now().Second()
+
+		for queueIndx, qe := range Queue {
+
+			//if there is no message for that element
+			if qe.lastTimeVisible == -1 {
+				continue
+			}
+
+			messTO := qe.lastTimeVisible + TIME_OUT
+
+			//if TO oof the queue element has expired, then send again the message
+			if now >= messTO {
+
+				//send the queue element to the consumer that has not received it
+				for consID, vis := range qe.visible {
+
+					c := getConsumer(consID)
+					if c.ID == -1 {
+						continue
+					}
+					if vis != SENT {
+						go sendToConsumer(queueIndx, c)
+						//TODO handle eventual prediction of crashed consumer
+						//by increasing a counter...
+					}
+				}
+
+			}
+		}
+	}
+}
+
+func serverRPC() {
+
+	//the consumer has to send a message in which they specify
+	//the port and  the ip address that are going to be used
+	//by the broker to send them the message of the queue
+
+	//setting up RPC server
+	inbound, err := net.Listen("tcp", ":"+port)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go TimeOutChecker()
+
+	queueManager := new(QueueManager)
+	rpc.Register(queueManager)
+
+	rpc.Accept(inbound)
+}
+
+func main() {
+
+	fmt.Println("Insert time out value (in seconds): ")
+	fmt.Scanf("%d", &TIME_OUT)
+
+	fmt.Println("Insert RPC server PORT: ")
+	fmt.Scanf("%s", &port)
+
+	consumerNum = 0
+
+	//setting up queue, 'lastTimeVisible' set to -1
+	for i := 0; i < QUEUE_LEN; i++ {
+
+		//qe.visible = []bool{}
+		//the set up of the array of bool depends on how many
+		//consumer there are, so it has to be done by the InsertMessage routine
+		Queue[i].lastTimeVisible = -1
+	}
+
+	go serverRPC()
+
+	//TODO if does not work properly just remove
+	//the user can edit the TIME_OUT value, while the broker is working
+	for {
+		fmt.Println("Insert new TIME_OUT value: ")
+		fmt.Scanf("%d", &TIME_OUT)
+	}
+
+}
---
+++ producer/producer.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"fmt"
+	"net/rpc"
+	"os"
+	"time"
+)
+
+type ProducerInfo struct {
+
+	//ip and port are going to be read and used by the producer
+	//to establish a connection with the broker
+	ip              string
+	port            string
+	messagesChannel chan string
+	ctrlChannel     chan int
+
+}
+
+const (							//used in RR1 mechanism
+	TCP_STYLE = 1				//each request is sent every 1s, 2s, 4s, 8s... MAX_SEC
+	BRUTE_FORCE_STYLE = 2		//never stop trying to send the message
+	MAX_TRIES_STYLE = 3			//do at most MAX_TRIES_STYLE tries
+
+	MAX_SEC = 64
+
+	GOR_EXIT = 0				//used to request the closure of the "Produce" go routine
+)
+
+//TODO producer list is a critical section
+var producerList []ProducerInfo
+var serviceMethod string
+var rr1Mode int
+var maxTimes int
+
+func DialRR1(mess, ip, port string) {
+
+	var reply string
+
+	client, err := rpc.Dial("tcp", ip+":"+port)
+
+	if err != nil {
+		fmt.Println("Something went wrong...\n"+err.Error())
+		return
+	}
+
+	if rr1Mode == TCP_STYLE {
+
+		for sec := time.Second * 1; sec <= MAX_SEC; sec = sec *2 {
+
+			err = client.Call(serviceMethod, mess, &reply)
+
+			if err != nil && sec == MAX_SEC {
+				fmt.Println("Could not send the message\n"+err.Error())
+				return
+			} else if err == nil && sec < MAX_SEC {
+				break
+			} else {
+				continue
+			}
+		}
+
+	} else if rr1Mode == BRUTE_FORCE_STYLE {
+
+		for {
+			err = client.Call(serviceMethod, mess, &reply)
+
+			if err == nil {
+				break
+			}
+		}
+
+	} else if rr1Mode == MAX_TRIES_STYLE {
+
+		for i := 1; i <= maxTimes; i++ {
+
+			err = client.Call(serviceMethod, mess, &reply)
+			if err != nil && i == maxTimes {
+				fmt.Println("Could not send the message\n"+err.Error())
+				return
+			} else if err == nil && maxTimes < MAX_SEC {
+				break
+			} else {
+				continue
+			}
+		}
+
+	} else {
+
+		fmt.Println("Invalid rr1Mode inserted")
+		return
+	}
+
+	fmt.Printf("Go Routine %d: message sent.\n", os.Getgid())
+	//TODO close channel
+}
+
+//the actual function that will listen to a given channel
+//and when a message is received the DialRR1 mechanism will start
+//there will be as many go routine of the Produce func as the brokers connected to the producer
+func Produce(info ProducerInfo) {
+
+	for {
+		select{
+		case cmd := <- info.ctrlChannel:
+			if cmd == GOR_EXIT {
+				return
+			}
+		case msg := <- info.messagesChannel:
+			DialRR1(msg, info.ip, info.port)
+		}
+	}
+
+}
+
+func AddBroker() {
+
+	var ip, port string
+	fmt.Println("Insert broker IP: ")
+	fmt.Scanf("%s:%s", &ip)
+	fmt.Println("Insert broker Port: ")
+	fmt.Scanf("%s", &port)
+
+	ch := make(chan string)
+	ctrlCh := make(chan int)
+
+	producerList = append(producerList, ProducerInfo{ip, port, ch, ctrlCh})
+
+	//create go routine associated to the broker
+	go Produce(producerList[len(producerList)-1])
+
+}
+
+func RemoveBroker() {
+
+	var choice int
+
+	fmt.Println("Which broker do you want to delete ?")
+	BrokerList()
+
+	fmt.Println("Your choice: ")
+	fmt.Scanf("%d", &choice)
+	fmt.Printf("You've choosed %d\n", choice)
+
+	//send kill message to kill the goroutine associated to the broker.... work on this
+	producerList[choice].ctrlChannel <- GOR_EXIT
+
+	close(producerList[choice].ctrlChannel)
+	close(producerList[choice].messagesChannel)
+
+	producerList[choice] = producerList[len(producerList)-1] // Copy last element to index i.
+	producerList = producerList[:len(producerList)-1] // Truncate slice
+
+}
+
+func BrokerList() {
+
+	fmt.Println("Broker list:")
+	for indx, pl := range producerList {
+		fmt.Printf("%d) %s:%s\n", indx, pl.ip, pl.port)
+	}
+
+}
+
+func rr1ModeSelector() {
+
+	for {
+		fmt.Println("What type of request retransmit do you want to use ?\n")
+
+		fmt.Println("1)TCP_STYLE")
+		fmt.Println("2)BRUTE_FORCE_STYLE")
+		fmt.Println("3)MAX_TRIES_STYLE")
+
+		fmt.Scanf("%d", &rr1Mode)
+
+		if rr1Mode < 1 && rr1Mode > 3 {
+			fmt.Println("Invalid input, retry")
+			continue
+		}
+
+		if rr1Mode == MAX_TRIES_STYLE {
+			fmt.Println("How many times do you want to try to reach the queque ?\n")
+			fmt.Scanf("%d", &maxTimes)
+		}
+
+		break
+	}
+
+}
+
+//go routine that will take messages from stdin
+//and send to all the channels of all the broker that the producer is connected to
+func Selection() {
+
+	var choice int
+	var message string
+
+	fmt.Println("What service method do you want to execute ?\nServiceMethod: ")
+	fmt.Scanf("%s", &serviceMethod)
+
+	rr1ModeSelector()
+
+	for {
+		fmt.Println("What do you want to do ?")
+		fmt.Println("1)Print BrokerList")
+		fmt.Println("2)Remove a Broker")
+		fmt.Println("3)Add a Broker")
+		fmt.Println("4)Produce a message")
+		fmt.Println("5)Select a RR1 Mode")
+		fmt.Println("6)Exit")
+
+		fmt.Scanf("%d", &choice)
+
+		switch choice{
+			case 1:
+				BrokerList()
+				break
+			case 2:
+				RemoveBroker()
+				break
+			case 3:
+				AddBroker()
+				break
+			case 4:
+				fmt.Println("Insert the message that you want to produce: ")
+				fmt.Scanf("%s", &message)
+
+				//Inserting message in all broker's channels
+				for _, p := range producerList {
+					p.messagesChannel <- message
+				}
+			case 5:
+				rr1ModeSelector()
+			case 6:
+				return
+
+			default:
+				fmt.Println("What ?")
+		}
+	}
+
+}
+
+
+func main() {
+	Selection()
+
+}
